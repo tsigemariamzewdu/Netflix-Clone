038d4251efd96568fd27f48f89159eabd867dfd1
tsigemariamzewdu <tsigemariamzewdu20@gmail.com>
2024-10-30 10:46:24 +0300

    header and footer

---
+++ netflix-clone/src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';  // Ensure Footer exists
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
