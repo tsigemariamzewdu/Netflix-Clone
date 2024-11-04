38d38e9eb9c35748ac1122d3bc07dfbf55686d90
tsigemariamzewdu <tsigemariamzewdu20@gmail.com>
2024-11-04 05:45:22 +0300

    banner

--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,4 +1,5 @@
-const API_KEY=process.REACT_APP_API_KEY;
+
+const API_KEY=process.env.REACT_APP_API_KEY;
 const requests={
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
@@ -10,3 +11,4 @@ const requests={
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
     fetchPopularTVShows: `/tv/popular?api_key=${API_KEY}&language=en-US`,
 }
+export default requests;
